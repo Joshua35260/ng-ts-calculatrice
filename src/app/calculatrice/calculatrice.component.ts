c96b2b80669d8a925ac214ae33cf36c2afbbcb11
Joshua Dupin <jdupin@topics.fr>
2023-02-15 13:23:04 +0100

    change my name

--- src/app/calculatrice/calculatrice.component.ts
+++ src/app/calculatrice/calculatrice.component.ts
@@ -3,21 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calculatrice',
   templateUrl: './calculatrice.component.html',
-  styleUrls: ['./calculatrice.component.css']
+  styleUrls: ['./calculatrice.component.css'],
 })
-
-
 export class CalculatriceComponent {
-   
-  number1: number=0;
-  number2: number=0;
-  operator: string= '';
+  number1: number = 0;
+  number2: number = 0;
+  operator: string = '';
   plus: string = '+';
   moins: string = '-';
-  multipliate: string = '*' ;
-  divide: string = '/' ;
+  multipliate: string = '*';
+  divide: string = '/';
   result: any = 0;
-
 
   insertPlus() {
     this.operator = '+';
@@ -35,7 +31,7 @@ export class CalculatriceComponent {
   random1() {
     this.number1 = Math.floor(Math.random() * 101);
   }
-  
+
   random2() {
     this.number2 = Math.floor(Math.random() * 101);
   }
@@ -59,7 +55,5 @@ export class CalculatriceComponent {
     this.number2 = 0;
     this.operator = '';
     this.result = 0;
-
   }
-
 }
